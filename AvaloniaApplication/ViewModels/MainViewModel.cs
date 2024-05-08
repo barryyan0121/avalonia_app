7736f48dce581ae40c7ea82a38ef8338e780188a
Han Yan <yanhan0121@gmail.com>
2024-05-08 14:45:11 +0800

    add complete view

--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -8,30 +8,24 @@ namespace AvaloniaApplication.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private AvaloniaList<ProductionData> _tableData = [];
-
     public MainViewModel()
     {
         const string connectionString =
             "Server=localhost;Port=3306;Database=sample_db;Uid=sample_user;Pwd=sample_password;";
         var databaseManager = new DatabaseManager(connectionString);
-        _tableData = databaseManager.LoadData();
+        TableData = databaseManager.LoadData();
         // Start a background task to periodically check for data changes
         Task.Run(async () => { await CheckForDataChanges(databaseManager); });
     }
 
-    public AvaloniaList<ProductionData> TableData
-    {
-        get => _tableData;
-        set => this.RaiseAndSetIfChanged(ref _tableData, value);
-    }
+    public AvaloniaList<ProductionData> TableData { get; private set; }
 
     private async Task CheckForDataChanges(DatabaseManager databaseManager)
     {
         while (true)
         {
             // Reload data from the database
-            _tableData = databaseManager.LoadData();
+            TableData = databaseManager.LoadData();
             this.RaisePropertyChanged(nameof(TableData)); // Notify UI about the data change
             await Task.Delay(TimeSpan.FromSeconds(10)); // Wait for 10 seconds before reloading data again
         }
