8363faafe51f1968f0558d269d61942c645642dc
Han Yan <yanhan0121@gmail.com>
2024-05-08 17:57:51 +0800

    add grid layout and fix datagrid; create datatype for weekly production management

--- AvaloniaApplication/Models/DatabaseManager.cs
+++ AvaloniaApplication/Models/DatabaseManager.cs
@@ -14,13 +14,12 @@ public class DatabaseManager
 
     public AvaloniaList<ProductionData> LoadData()
     {
-        var tableData = new AvaloniaList<ProductionData>();
-
         using var connection = new MySqlConnection(_connectionString);
         connection.Open();
-        var query = "SELECT * FROM production_data";
+        var query = "SELECT * FROM production_data WHERE DATE(date) = CURDATE() + 2;";
         var command = new MySqlCommand(query, connection);
         var reader = command.ExecuteReader();
+        var data = new AvaloniaList<ProductionData>();
 
         while (reader.Read())
         {
@@ -32,13 +31,48 @@ public class DatabaseManager
                 DefectiveCount = reader.GetInt32("defective_count"),
                 QualifiedRate = reader.GetDouble("qualified_rate"),
                 TotalCount = reader.GetInt32("total_count"),
-                StartDate = reader.GetDateTime("start_date"),
-                EndDate = reader.GetDateTime("end_date")
+                Date = reader.GetDateTime("date")
             };
 
-            tableData.Add(productionData);
+            data.Add(productionData);
         }
 
-        return tableData;
+        return data;
+    }
+
+    public AvaloniaList<WeeklyProductionData> LoadWeeklyData()
+    {
+        using var connection = new MySqlConnection(_connectionString);
+        connection.Open();
+        var query =
+            "SELECT name, MIN(date) AS start_date, " +
+            "MAX(date) AS end_date," +
+            "SUM(qualified_count) AS total_qualified_count, " +
+            "SUM(defective_count) AS total_defective_count, " +
+            "SUM(total_count) AS total_total_count " +
+            "FROM production_data WHERE date >= CURDATE() - INTERVAL 6 DAY " +
+            "AND date < CURDATE() + INTERVAL 6 DAY GROUP BY name;";
+        var command = new MySqlCommand(query, connection);
+        var reader = command.ExecuteReader();
+        var data = new AvaloniaList<WeeklyProductionData>();
+
+        while (reader.Read())
+        {
+            var productionData = new WeeklyProductionData
+            {
+                Name = reader.GetString("name"),
+                StartDate = reader.GetDateTime("start_date"),
+                EndDate = reader.GetDateTime("end_date"),
+                TotalQualifiedCount = reader.GetInt32("total_qualified_count"),
+                TotalDefectiveCount = reader.GetInt32("total_defective_count"),
+                TotalCount = reader.GetInt32("total_total_count"),
+                TotalQualifiedRate = (double)reader.GetInt32("total_qualified_count") /
+                                     reader.GetInt32("total_total_count")
+            };
+
+            data.Add(productionData);
+        }
+
+        return data;
     }
 }
--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -2,31 +2,54 @@
 using System.Threading.Tasks;
 using Avalonia.Collections;
 using AvaloniaApplication.Models;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 using ReactiveUI;
 
 namespace AvaloniaApplication.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    private DateTime _today = DateTime.Today;
+
     public MainViewModel()
     {
         const string connectionString =
             "Server=localhost;Port=3306;Database=sample_db;Uid=sample_user;Pwd=sample_password;";
         var databaseManager = new DatabaseManager(connectionString);
-        TableData = databaseManager.LoadData();
+        DailyData = databaseManager.LoadData();
+        WeeklyData = databaseManager.LoadWeeklyData();
         // Start a background task to periodically check for data changes
         Task.Run(async () => { await CheckForDataChanges(databaseManager); });
     }
 
-    public AvaloniaList<ProductionData> TableData { get; private set; }
+    public AvaloniaList<ProductionData> DailyData { get; private set; }
+    public AvaloniaList<WeeklyProductionData> WeeklyData { get; private set; }
+
+    public ISeries[] Series { get; set; } =
+    [
+        new LineSeries<double>
+        {
+            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+            Fill = null
+        }
+    ];
+
+    public DateTime Today
+    {
+        get => _today;
+        set => this.RaiseAndSetIfChanged(ref _today, value);
+    }
 
     private async Task CheckForDataChanges(DatabaseManager databaseManager)
     {
         while (true)
         {
             // Reload data from the database
-            TableData = databaseManager.LoadData();
-            this.RaisePropertyChanged(nameof(TableData)); // Notify UI about the data change
+            DailyData = databaseManager.LoadData();
+            WeeklyData = databaseManager.LoadWeeklyData();
+            this.RaisePropertyChanged(nameof(DailyData)); // Notify UI about the data change
+            this.RaisePropertyChanged(nameof(WeeklyData)); // Notify UI about the data change
             await Task.Delay(TimeSpan.FromSeconds(10)); // Wait for 10 seconds before reloading data again
         }
     }
