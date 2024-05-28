1c5e860fb1243219a8fe161080cb40b58588b543
Han Yan <yanhan0121@gmail.com>
2024-05-28 09:25:24 +0800

    separate user controls from view files

---
+++ AvaloniaApplication/Views/SecondGridControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication.Views;
+
+public partial class SecondGridControl : UserControl
+{
+    public SecondGridControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AvaloniaApplication/Views/ThirdGridControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication.Views;
+
+public partial class ThirdGridControl : UserControl
+{
+    public ThirdGridControl()
+    {
+        InitializeComponent();
+    }
+}
